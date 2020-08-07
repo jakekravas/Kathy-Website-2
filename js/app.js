e77126bf1572abbfd6311ccd5bd6c309ffc670b3
Jake Kravas <jakekravas7@gmail.com>
2020-08-07 05:03:07 -0500

    Refined smooth scroll

--- js/app.js
+++ js/app.js
@@ -1,14 +1,26 @@
-$('.carousel').carousel({
-  interval: 4000,
-  keyboard: true,
-  pause: 'hover',
-  wrap: true
-});
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
 
-$('#slider4').on('slide.bs.carousel', function () {
-  console.log('SLIDE!');
-});
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
 
-$('#slider4').on('slid.bs.carousel', function () {
-  console.log('SLID!');
+      // Store hash
+      let hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // 800 = number of milliseconds it takes to scroll to specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top - 100
+      }, 800);
+    }
+  });
+  $('.carousel').carousel({
+    interval: 4000,
+    keyboard: true,
+    pause: 'hover',
+    wrap: true
+  });
 });
